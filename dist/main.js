42f0b7d44a55983845d23f9872ff291d94521b0e
Shabaz Farooq <shabazf@amazon.com>
2018-10-12 14:33:08 -0700

    Adding temp build utilities

--- dist/main.js
+++
@@ -1,8 +0,0 @@
-console.log('hello world');
-console.log('__filename: ' + __filename);
-console.log('__dirname : ' + __dirname);
-console.log('process.argv : ' + process.argv);
-function opposite(x) {
-    if (x === void 0) { x = { hello: { world: 'hi' } }; }
-    return x.hello.world;
-}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
-console.log('hello world');
+const commandLineArgs = require('command-line-args');
+const command_init = require('./commands/init');
+const optionDefinitions = [
+  { name: 'verbose', alias: 'v', type: Boolean },
+  { name: 'src', type: String, multiple: true, defaultOption: true },
+  { name: 'timeout', alias: 't', type: Number },
 
-console.log('__filename: ' + __filename);
-console.log('__dirname : ' + __dirname);
-console.log('process.argv : ' + process.argv);
+  { name: 'init', alias: 'i', type: Number }
+];
+const options = commandLineArgs(optionDefinitions);
 
-function opposite(x={hello:{world:'hi'}}){
-  return x.hello.world;
-}
+console.log(
+  JSON.stringify(options, null, 2)
+);
+
+// console.log('__filename: ' + __filename);
+// console.log('__dirname : ' + __dirname);
+// console.log('process.argv : ' + process.argv);
+
+// function opposite(x={hello:{world:'hi'}}){
+//   return x.hello.world;
+// }
